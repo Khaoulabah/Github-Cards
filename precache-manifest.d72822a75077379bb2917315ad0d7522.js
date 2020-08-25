22c517086c583582dbb91dc402306fff82a6b9d9
Khaoula Bahloul <khaoula@uw.edu>
2020-08-25 01:09:32 -0700

    Updates

---
+++ precache-manifest.d72822a75077379bb2917315ad0d7522.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bee39d619e3b54a8934ee7c59b6ffcf7",
+    "url": "/Github-Cards/index.html"
+  },
+  {
+    "revision": "72fb7031947a3aec7580",
+    "url": "/Github-Cards/static/css/main.757eee48.chunk.css"
+  },
+  {
+    "revision": "483f63504b7e073f0cec",
+    "url": "/Github-Cards/static/js/2.51bb99da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Github-Cards/static/js/2.51bb99da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72fb7031947a3aec7580",
+    "url": "/Github-Cards/static/js/main.8e551608.chunk.js"
+  },
+  {
+    "revision": "e60c925c0b41001d39b3",
+    "url": "/Github-Cards/static/js/runtime-main.c752d3ad.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Github-Cards/static/media/logo.5d5d9eef.svg"
+  }
+]);
